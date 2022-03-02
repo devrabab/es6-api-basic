b6d9412df844b3bf649c6c716b11851b0db7d8b3
Rabab Noor <rababnoor28@gmail.com>
2022-03-02 09:59:02 +0600

    parameter

---
+++ default-parameter.js
@@ -0,0 +1,6 @@
+function add(num1, num2){
+    const total = num1 + num2;
+    return total;
+}
+const result = add(45, 65);
+console.log(result);
---
+++ let-const.js
@@ -0,0 +1,10 @@
+let balance = 1240;
+balance = 1340;
+
+const userName = 'RababNoor';
+const weTogether = 'ami' + userName;
+console.log(weTogether);
+
+const numbers = [34, 567, 67, 12];
+// numbers = [678, 354, 11]; not allowed
+numbers.push(555);
