0757b3877ce8145c1777f126e30b95d6f50f1616
Rabab Noor <rababnoor28@gmail.com>
2022-03-02 09:29:07 +0600

    fs

---
+++ basic.js
@@ -0,0 +1,37 @@
+// variable 
+var deposit = 400;
+// condition 
+if (deposit > 500) {
+
+}
+if (deposit == 500) {
+     
+}
+else if (deposit != 400){
+
+}
+// array 
+const numbers = [45, 67, 12, 89, 23];
+const numberCount = numbers.length; 
+numbers.push(111);
+numbers.pop();
+numbers[5] = 222;
+// check whether 222 included in the array 
+if(numbers.indexOf(222) != -1){
+     
+}
+if (numbers.includes(222)){
+
+}
+// loop
+// while, for 
+for (const number of numbers) {
+     
+}
+// function
+function fullName (first, second) {
+    const name = first + ' ' + second;
+}
+const person = fullName('Rahim', 'Ali');
+// Object
+const bottle = {color: 'yellow', contain: 'water', price: 500}
